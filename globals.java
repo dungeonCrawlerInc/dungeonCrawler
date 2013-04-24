bf82c87357976ab2f7832815347a4588b9d3ef5d
Andrew Boutin <andrew.w.boutin@gmail.com>
2013-04-24 18:04:01 -0300

    Update globals.java

--- globals.java
+++ globals.java
@@ -1,4 +1,26 @@
-//update me
+// Map key struct
+
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+
+
 public class Globals{
-   public static final char USER='U', BARTENDER='B', STOOL='S', TABLE='T', WALL='W', DOOR='D', ENEMY='E', FLOOR='F';
+ 
+   private Color brown = new Color(50, 50 , 50);
+   public static MapChar USER, BARTENDER, STOOL, TABLE, WALL, DOOR, ENEMY, FLOOR;
+
+   public Globals(){
+      USER = new MapChar('U', Color.BLACK);
+      BARTENDER = new MapChar('B', Color.BLACK);
+      STOOL = new MapChar('S', brown);
+      TABLE = new MapChar('T', brown); 
+      WALL = new MapChar('W', Color.GRAY);
+      DOOR = new MapChar('D', brown);
+      ENEMY = new MapChar('E', Color.RED);
+      FLOOR = new MapChar('F', Color.WHITE);
+   }
+
 }
+
