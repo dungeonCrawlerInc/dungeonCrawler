8d93affb111ea05b2e46bb11a4db70aca4473b76
Andrew Boutin <andrew.w.boutin@gmail.com>
2013-04-24 12:04:49 -0300

    Update room.java

--- room.java
+++ room.java
@@ -1,4 +1,30 @@
-name
-dimensions
-contents
+// name
+// dimensions
+// contents
 
+// Imports
+import java.util.*;
+
+// 
+public class room{
+  
+  // size of the map
+  private int _rows = 10;
+  private int _columns = 10;
+  private char _numAdjRooms = 0; // Accesible rooms from current room
+  
+  // Grid for room
+  private char[][] _roomGrid;
+
+  // array of adjecent room addresses
+  
+}
+
+void createRoom(int rows, int columns){
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < columns; j++)
+      _roomGrid[rows][columns] = 'F';
+  }
+}
+
+// Keep room tree somewhere...
