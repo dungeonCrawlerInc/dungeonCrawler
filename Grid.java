95503b0339e99116a4cde12933e8ae59f3ea1685
Andrew Boutin <andrew.w.boutin@gmail.com>
2013-04-25 16:11:51 -0300

    Changed name from Map to Grid

--- Grid.java
+++ Grid.java
@@ -1,5 +1,5 @@
-/* Map class is what map objects will get put into to populate the map
- * Simulates the terrain, walkable space, etc.
+/* Grid class is what grid objects will get put into to populate the grid
+ * Simulates the map, building, terrain, walkable space, items, people, etc.
  */
 
 // Imports
@@ -9,47 +9,47 @@ import java.awt.*;
 
 
 /* Notes for future:
- *  Keep map tree
- *  Keep array of adjecent map addresses
+ *  Keep grid tree
+ *  Keep array of adjecent grid addresses
  */
 
 
 // 
-public class Map extends JPanel{
+public class Grid extends JPanel{
   
    public static final int _GridSize = 10; // Size of an individual square
    
-   private int _rows = 100; // Size of the available map
+   private int _rows = 100; // Size of the available grid
    private int _columns = 100;
-   private char _numAdjMaps = 0; // Accesible rooms from current room
+   private char _numAdjGrids = 0; // Accesible rooms from current room
   
    
-   private MapChar[][] _mapGrid; // Grid for room
+   private GridChar[][] _grid; // Grid for room
    
 
    // Room constructor
-   public Map(){
+   public Grid(){
      
-      this._mapGrid = new MapChar[_rows][_columns]; // Initialize the grid
+      this._grid = new GridChar[_rows][_columns]; // Initialize the grid
       
-      // Populate the map with all floor objects, can be changed later
+      // Populate the grid with all floor objects, can be changed later
       for(int i = 0; i < rows; ++i){
          for(int j = 0; j < columns; ++j)
-            _mapGrid[i][j] = Globals.FLOOR;
+            _grid[i][j] = Globals.FLOOR;
       }
       
-      // Set the dimensions of the overall room
-      int mapWidth = columns * _GridSize;
-      int mapHeight = rows * _GridSize;
-      setPreferredSize(new Dimension(mapWidth, mapHeight));
+      // Set the dimensions of the overall grid
+      int gridWidth = columns * _GridSize;
+      int gridHeight = rows * _GridSize;
+      setPreferredSize(new Dimension(gridWidth, gridHeight));
    }
 
   // Temporary method, will go into the play area gui for it to call.
   public void setTavern(){
       for(int i = 0; i < _rows; i++)
-         _mapGrid[i][0] = _mapGrid[i][_columns - 1] = Globals.WALL;
+         _grid[i][0] = _grid[i][_columns - 1] = Globals.WALL;
       for(int i = 0; i < _columns; i++)
-         _mapGrid[0][i] = _mapGrid[_rows - 1][i] = Globals.WALL;
+         _grid[0][i] = _grid[_rows - 1][i] = Globals.WALL;
    }
    
    @Override
@@ -62,22 +62,22 @@ public class Map extends JPanel{
       int recW = getWidth() / _rows; // Draw the grid
       int recH = getHeight() / _columns;
       
-      for (int i = 0; i < _rows; i++){ // Determine what to draw for each grid square on map
+      for (int i = 0; i < _rows; i++){ // Determine what to draw for each grid square on grid
          for (int j = 0; j < _columns; j++){
             
             int xCord = i * recW; // Upper left corner of this terrain rect
             int yCord = j * recH;
             
             
-            if(_roomGrid[i][j] == Globals.VOID){ // Paint black grid for void space
+            if(_grid[i][j] == Globals.VOID){ // Paint black grid for void space
               g.setColor(Color.BLACK); // All void space is black
               g.drawRect(xCord, yCord, recW, recH); // Fill in the square with black
             }
             
              // Don't paint anything for floor space. Void space already painted
-            if(_roomGrid[i][j] != Globals.FLOOR && _roomGrid[i][j] != Globals.VOID){
-               g.setColor(_roomGrid[i][j].getColor()); // Get color of map object
-               String s = "" + _roomGrid[i][j].getChar(); // Grab the character from the map object
+            if(_grid[i][j] != Globals.FLOOR && _grid[i][j] != Globals.VOID){
+               g.setColor(_grid[i][j].getColor()); // Get color of map object
+               String s = "" + _grid[i][j].getChar(); // Grab the character from the map object
                g.drawString(s, xCord, yCord); // Paint the character in the appropriate color on the grid square
             }
             
@@ -89,10 +89,10 @@ public class Map extends JPanel{
       SwingUtilities.invokeLater(new Runnable(){
          public void run(){
             Globals globals = new Globals();
-            JFrame frame = new JFrame("Room");
-            Room room = new Room();
-            room.setTavern();
-            frame.add(room);
+            JFrame frame = new JFrame("Grid");
+            Grid grid = new Grid();
+            grid.setTavern();
+            frame.add(grid);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
