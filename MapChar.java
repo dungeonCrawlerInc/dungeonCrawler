628ddf97e53813ff8469565b4c829401923869d9
Andrew Boutin <andrew.w.boutin@gmail.com>
2013-04-25 14:21:48 -0300

    Update MapChar.java

--- MapChar.java
+++ MapChar.java
@@ -1,33 +1,35 @@
-
+//Imports
 import java.util.*;
 
-
-
-
+// MapChar is an object that get placed on the map
 public class MapChar{
    
-   private java.awt.Color _color;
-   private char _character;
+   private java.awt.Color _color; // Color to paint object
+   private char _character; // ASCI char to display for object
    
    public MapChar(char character, java.awt.Color color){
-      _character = character;
+      _character = character; // Set the variables based on parameters for constructor
       _color = color;
    }
 
+   // Can change color incase object gets changed to another object or current object's state changes
    public void setColor(java.awt.Color c){
       _color = c;
    }
 
+   // Accesor to color of object
    public java.awt.Color getColor(){
       return _color;
    }
    
+   // In game possibility to change what is currently at spot on map
    public void setChar(char character){
       _character = character;
    }
    
+   // Can be used to determine what object is at a given location
    public char getChar(){
       return _character;
    }
 
-}
+} // End of file
