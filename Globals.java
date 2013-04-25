6814c3f6a04f78a61572d204b19fc81bfeb461e5
Andrew Boutin <andrew.w.boutin@gmail.com>
2013-04-25 11:13:32 -0300

    Update Globals.java

--- Globals.java
+++ Globals.java
@@ -1,18 +1,16 @@
-// Map key struct
-
-import java.util.*;
+import java.util.*; // Might be able to get rid of some of these?
 import java.awt.*;
 import javax.swing.*;
 
 
-
+// Should probably rename file and class to MapCharacters
 public class Globals{
  
-   private Color brown = new Color(50, 50 , 50);
-   public static MapChar USER, BARTENDER, STOOL, TABLE, WALL, DOOR, ENEMY, FLOOR;
+   private Color brown = new Color(50, 50 , 50); // Not actually brown, will have to test
+   public static MapChar USER, BARTENDER, STOOL, TABLE, WALL, DOOR, ENEMY, FLOOR; // Accesible by other classes/files
 
-   public Globals(){
-      USER = new MapChar('U', Color.BLACK);
+   public Globals(){ // When adding in a new item/thing to go on map just give it a name and call constructor
+      USER = new MapChar('U', Color.BLACK); // Constructor takes an ASCI char and a color.
       BARTENDER = new MapChar('B', Color.BLACK);
       STOOL = new MapChar('S', brown);
       TABLE = new MapChar('T', brown); 
