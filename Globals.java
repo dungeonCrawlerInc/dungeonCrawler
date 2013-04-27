d43a8050b025dacdc01b454441205bc42bdeac25
Andrew Boutin <andrew.w.boutin@gmail.com>
2013-04-26 21:28:01 -0300

    Update Globals.java
    Changed mapChars to gridChars in constructor.

--- Globals.java
+++ Globals.java
@@ -1,6 +1,4 @@
-import java.util.*; // Might be able to get rid of some of these?
 import java.awt.*;
-import javax.swing.*;
 
 public class Globals{ // Should probably later change to MapChars.... unless other classes get added in here
  
@@ -8,15 +6,15 @@ public class Globals{ // Should probably later change to MapChars.... unless oth
    public static GridChar USER, BARTENDER, STOOL, TABLE, WALL, DOOR, ENEMY, FLOOR, VOID; // Accesible by other classes/files
 
    public Globals(){ 
-      USER = new MapChar('U', Color.BLACK); // Constructor takes an ASCI char and a color.
-      BARTENDER = new MapChar('B', Color.BLACK);
-      STOOL = new MapChar('S', brown);
-      TABLE = new MapChar('T', brown); 
-      WALL = new MapChar('W', Color.GRAY);
-      DOOR = new MapChar('D', brown);
-      ENEMY = new MapChar('E', Color.RED);
-      FLOOR = new MapChar('F', Color.WHITE); // Blank walkable space on grid
-      VOID = new MapChar('V', Color.BLACK); // Unused surrounding grid space
+      USER = new GridChar('U', Color.BLACK); // Constructor takes an ASCI char and a color.
+      BARTENDER = new GridChar('B', Color.BLACK);
+      STOOL = new GridChar('S', brown);
+      TABLE = new GridChar('T', brown); 
+      WALL = new GridChar('W', Color.GRAY);
+      DOOR = new GridChar('D', brown);
+      ENEMY = new GridChar('E', Color.RED);
+      FLOOR = new GridChar('F', Color.WHITE); // Blank walkable space on grid
+      VOID = new GridChar('V', Color.BLACK); // Unused surrounding grid space
       
       // To add a new GridChar, give it a name and call constructor with desired ASCI char and color
       // Note: Update README file with new GridChar.  Also have to add GridChar name to public static list above
