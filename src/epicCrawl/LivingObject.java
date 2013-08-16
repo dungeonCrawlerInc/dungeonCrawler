8f517cde2b59e794e1fac6d26b54b0e88e00dd0c
Greg Hilston <Grehgh@gmail.com>
2013-08-16 01:15:36 -0400

    Added a few getters and setters and moved the Character Designer to an appropriatee spot

--- src/epicCrawl/LivingObject.java
+++ src/epicCrawl/LivingObject.java
@@ -6,7 +6,7 @@ package epicCrawl;
 // Any object in the game that is alive is derived from this class.
 public class LivingObject extends GridObject implements ClickableObject{
     private boolean _isAlive;
-    private int _xLoc, _yLoc;
+    private int _xLoc, _yLoc, _health, _strength, _dexterity, _intelligence;
 
     public LivingObject(String name, int xLoc, int yLoc){
         super(name, false);
@@ -14,6 +14,12 @@ public class LivingObject extends GridObject implements ClickableObject{
         _xLoc = xLoc;
         _yLoc = yLoc;
         _isAlive = true;
+
+        //Default parameters (Any class that implements this class should over write these values)
+        _health = 10;
+        _strength = 10;
+        _dexterity = 10;
+        _intelligence = 10;
     }
 
     public boolean isAlive(){
@@ -35,6 +41,38 @@ public class LivingObject extends GridObject implements ClickableObject{
 
     public int getYLoc(){
         return _yLoc;
+    }
+
+    public int getHealth() {
+        return _health;
+    }
+
+    public void setHealth(int _health) {
+        this._health = _health;
+    }
+
+    public int getStrength() {
+        return _strength;
+    }
+
+    public void setStrength(int _strength) {
+        this._strength = _strength;
+    }
+
+    public int getDexterity() {
+        return _dexterity;
+    }
+
+    public void setDexterity(int _dexterity) {
+        this._dexterity = _dexterity;
+    }
+
+    public int getIntelligence() {
+        return _intelligence;
+    }
+
+    public void setIntelligence(int _intelligence) {
+        this._intelligence = _intelligence;
     }
 
     // Needed temporarily
