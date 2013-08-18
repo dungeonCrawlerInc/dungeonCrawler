0a4d6f48de4b910271503dd8085ab90ab310be17
Greg Hilston <Grehgh@gmail.com>
2013-08-18 15:34:11 -0400

    Did some slight work on  OO design.
    I need to figure out how to import a file from another package, also
    How should we store the data for attributes and skills? A bunch of ints seems bad, maybe a Map or HashTable?

--- Hero.java
+++ Hero.java
@@ -1,10 +1,11 @@
 /*
     * TODO List
         * Add skill tree
+        *
  */
 
 //Only one instance of this class will ever be instantiated, the playing character AKA PC
 
 public class Hero extends Humanoid {
-
+    int jumping = 10;   //Heroes have skills
 }
---
+++ src/characterLoader/CharacterLoader.java
@@ -0,0 +1,10 @@
+/*
+    * TODO List
+        * Figure out how to import Hero.java from the Root directory, instead of copying the file to this package
+ */
+
+package characterLoader;
+
+public class CharacterLoader {
+    Hero player = new Hero();
+}
